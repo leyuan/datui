fd6a914338c2268e4280ec8ce7b978c6e439c148
Haoran <whr0002@gmail.com>
2016-12-06 20:49:14 -0700

    获取个人档案

--- public/js/custom/tutor-profile.js
+++ public/js/custom/tutor-profile.js
@@ -1,25 +1,37 @@
-var COOKIE_NAME = 'doNotCheatPlz';
+(function(){
+  var config = {
+    apiKey: "AIzaSyCF3MunGpmlhR_dgFgTsopUu108Zx1XpBI",
+    authDomain: "datui-ea9e4.firebaseapp.com",
+    databaseURL: "https://datui-ea9e4.firebaseio.com",
+    storageBucket: "datui-ea9e4.appspot.com",
+    messagingSenderId: "200514863768"
+  };
+  firebase.initializeApp(config);
 
-/**
- * Tutor Keys are
- * {name, phone, email, courses, avatar, intro}
- */
-new Vue({
-  el: '#tutor-info',
-  beforeMount() {
-    var tutorId = location.hash.substring(1); // get rid of the # character
-    var url = `/api/tutor/${tutorId}`;
-    this.$http.get(url).then(function (res) {
-      if (res.body) {
-          var tutor = JSON.parse(res.body);
-          tutor.courses.sort();
-        this.$set(this.$data, 'tutor', tutor);
-        ga('send', 'event', 'Users', 'View', tutor.name);
-      }
-    });
-  },
-  data: {
-    tutor: {},
-    userEmail: ''
-  },
-});
+  var COOKIE_NAME = 'doNotCheatPlz';
+
+  /**
+   * Tutor Keys are
+   * {name, phone, email, courses, avatar, intro}
+   */
+  new Vue({
+    el: '#tutor-info',
+    beforeMount() {
+      var self = this;
+      var tutorId = location.hash.substring(1); // get rid of the # character
+      var tutorRef = firebase.database().ref('tutors/' + tutorId).once('value').then(function(snapshot){
+        var tutorObj = snapshot.val();
+
+        if(tutorObj && tutorObj.details){
+          self.$set(self.$data, 'tutor', tutorObj.details);
+          ga('send', 'event', 'Users', 'View', tutorObj.details.name);
+        }
+      });
+    },
+    data: {
+      tutor: {},
+      userEmail: ''
+    },
+  });
+})();
+
